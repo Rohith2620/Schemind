f4428d4119d64818cdb48340bbaba541a0f32642
Rohith <rohithravi354@gmail.com>
2025-04-02 14:24:21 +0530

    feat: configure database connection

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Schemind Backend Server is Running');
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
